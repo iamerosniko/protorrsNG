066b71565de6b5d10a07d24d2be173416f244c14
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-10-10 16:48:58 -0400

    up

--- src/app/com_entities/app-settings.ts
+++ src/app/com_entities/app-settings.ts
@@ -1,8 +1,6 @@
 export class AppSettings {
-    public static CURRENT_URL = "http://localhost:52271/4399a480-e29b-4f6e-9408-962bad10991e/"
-    // private static CURRENT_URL = "http://projectworkplaceazure2.azurewebsites.net/api/"; 
-    //public static CURRENT_URL = "https://x.azurewebsites.net/api/"; 
-    //public static CURRENT_URL = "https://x.azurewebsites.net/api/"; 
+    public static CURRENT_URL = "http://protoreservation.azurewebsites.net/api/Reservations/api/"
+    
     public static GETAPIURL(controller:string):string{
         return this.CURRENT_URL+controller;
     }
